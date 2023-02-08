8a48fa5067fff9d62f48de5eb0dda51b917545a2
shaikshashavali <114045698+shaikshashavali@users.noreply.github.com>
2023-02-08 23:17:14 +0530

    output
    enter the value of b23
    sum of two numbers are:33
    
    ...Program finished with exit code 0
    Press ENTER to exit console.

---
+++ add two numbers.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+	#include<conio.h>
+	int main()
+	{
+	int a=10;
+	int b;
+	int sum;
+	printf("enter the value of b");
+	scanf("%d",&b);
+	sum=a+b;
+	printf("sum of two numbers are:%d",sum);
+	getch();
+	return 0;
+	}
